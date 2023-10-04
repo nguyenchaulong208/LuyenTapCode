0ee8d11ada0240bee90929a8c0b6d5a86b3cb68d
Theodore <113950597+nguyenchaulong208@users.noreply.github.com>
2023-10-04 22:37:27 +0700

    update
    bai 1

--- LuyenTap/LuyenTap/Program.cs
+++ LuyenTap/LuyenTap/Program.cs
@@ -10,6 +10,23 @@ namespace LuyenTap
     {
         static void Main(string[] args)
         {
+            //Menu:
+            Console.Write("Chon bai toan: ");
+            int bai = int.Parse(Console.ReadLine());
+            if(bai == 1)
+            {
+                // Bài 1: Tính S(n) = 1 + 2 + 3 + … +n
+                Console.WriteLine("Bai 1: Tinh S(n) = 1 + 2 + 3 + … + n");
+                XuLy.ThongBao();
+                int n = int.Parse(Console.ReadLine());
+                int kq = XuLy.TinhTong(n);
+                Console.WriteLine($"Ket qua la: {kq}");
+            }
+            if(bai == 2)
+            {
+                //Bài 2: Tính S(n) = 1 ^ 2 + 2 ^ 2 + … +n ^ 2
+            }
+
         }
     }
 }
---
+++ LuyenTap/LuyenTap/XuLy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuyenTap
+{
+    internal class XuLy
+    {
+        
+        public static void ThongBao()
+        {
+            Console.Write("Nhap n: ");
+        }
+        public static int TinhTong(int n)
+        {
+            int s = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                s = s + i;
+            }
+            return s;
+        }
+        
+    }
+}
