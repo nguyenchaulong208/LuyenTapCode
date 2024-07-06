16bd16959fb32b62193b0ff754e08acc3d68b488
Theodore <113950597+nguyenchaulong208@users.noreply.github.com>
2024-07-06 20:27:06 +0700

    up

--- LEETCODE/Algorithm1/Algorithm1/Program.cs
+++ LEETCODE/Algorithm1/Algorithm1/Program.cs
@@ -6,16 +6,48 @@ using System.Threading.Tasks;
 
 namespace Algorithm1
 {
-    internal class Program
+    public class Program
     {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+
+            int[] arr = new int[2];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        arr[0] = i;
+                        arr[1] = j;
+                        return arr;
+                        break;
+                    }
+                }
+            }
+            return null;
+        }
+    
+  
+            
         static void Main(string[] args)
         {
-            /*
-             * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-               You may assume that each input would have exactly one solution, and you may not use the same element twice.
-               can return the answer in any order.
-             */
+        /*
+         * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+           You may assume that each input would have exactly one solution, and you may not use the same element twice.
+           can return the answer in any order.
+         */
+        int[] arrNums = {2, 5, 5, 11};
+        int targetArr = 10;
+        int[] result = new int[2];
 
+            result= Algorithm1.Program.TwoSum(arrNums, targetArr);
+            foreach(var item in result)
+            {
+                Console.WriteLine(item);
+            }
+            
+            
         }
     }
 }
